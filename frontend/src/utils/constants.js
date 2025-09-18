081ef20139e8721d8b48e6b5fd237aaf96751275
Shaikh Ashfaq Shaikh Qayyum <khatikashfaq992@gmail.com>
2025-09-18 16:50:52 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://edviron-assignment-microservice-for-a.onrender.com';
 
 export const PAYMENT_STATUS = {
   SUCCESS: 'SUCCESS',
